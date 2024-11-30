b3c41a2834b2bd41b611851ae1e859b4387dbd33
ALEXAIORUSHUN <fsccasebox@gmail.com>
2024-11-30 20:40:12 +0100

    benzo-scoreboard
    Egy egyszerűsített Scoreboard rendszer, ami configolható

---
+++ benzo-scoreboard/shared/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Key = "f10"
+
+Config.ShowJob = true
+Config.ShowJobGrade = true 
+Config.ShowGroup = true
+Config.ShowPing = false
+Config.ShowID = false
+Config.ShowName = true
+Config.ShowHealth = false
+Config.ShowArmour = false
